7cdfb81ac1e86bb894c2b2fd4e02386b15e39418
Andrew <andrey-garanin-00@mail.ru>
2021-02-15 23:54:58 +0300

    Добавьте файлы проекта.

---
+++ Mandel/Mandel.h
@@ -0,0 +1,34 @@
+﻿
+// Mandel.h: основной файл заголовка для приложения Mandel
+//
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "включить pch.h до включения этого файла в PCH"
+#endif
+
+#include "resource.h"       // основные символы
+
+
+// CMandelApp:
+// Сведения о реализации этого класса: Mandel.cpp
+//
+
+class CMandelApp : public CWinApp
+{
+public:
+	CMandelApp() noexcept;
+
+
+// Переопределение
+public:
+	virtual BOOL InitInstance();
+
+// Реализация
+	UINT  m_nAppLook;
+	afx_msg void OnAppAbout();
+	DECLARE_MESSAGE_MAP()
+	virtual BOOL OnIdle(LONG lCount);
+};
+
+extern CMandelApp theApp;
